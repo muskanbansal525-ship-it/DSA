59c734c22432c9d5fbe37f528313851cee35f53a
muskan bansal <muskanbansal525@gmail.com>
2026-09-24 00:05:03 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (15.52%) - LeetSync

---
+++ 682-baseball-game/baseball-game.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        stack<int>st;
+     for(int i =0 ; i<=operations.size()-1;i++){
+         if (operations[i] == "+") {
+                int top1 = st.top(); st.pop();
+                int top2 = st.top();
+                st.push(top1);                
+                st.push(top1 + top2);          
+            }
+      else if ( operations[i]=="C" ){
+        st.pop();
+        }
+        else if ( operations[i]=="D"){
+            st.push(st.top()*2);
+        }
+        else  {
+                st.push(stoi(operations[i]));
+            }
+     }
+     int sum =0;
+     while ( !st.empty()){
+            sum += st.top();
+            st.pop();
+        }
+        return sum;
+     }
+};
